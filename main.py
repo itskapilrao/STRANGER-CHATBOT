f607d389673e509d2f4bdd119ee5c175c9d2ab9a
itskapilrao <itskapilrao@gmail.com>
2025-07-22 19:31:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ItsKapilYadav")
 SUPPORT_GRP = os.environ.get("SUPPORT_GRP", "KomalMusicRobotSupport")
 BOT_NAME = os.environ.get("BOT_NAME", "ShrutiChatBot")
 START_IMG = os.environ.get("START_IMG", "https://files.catbox.moe/48hczp.jpg")
-STKR = os.environ.get("STKR", "CAACAgUAAxkBAAIBV2S7R9yN1uPCvYbUjO_xuOgVLxbHAAK7AQACVp29VqIvA0XIWzwSLwQ")
+STKR = os.environ.get("STKR", "CAACAgIAAxkBAAIBg2S8dd6ICi9_yBrR7Ra6d_XH3mkdAAKnAQACVp29VjeHmTPJ1ZcRLwQ")
 
 StartTime = time.time()
 Mukesh = Client(
